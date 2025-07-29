25af8e6c424e565ecb88bf337571a02d01aa302f
Harshitha-DJ <171562486+Harshitha-DJ@users.noreply.github.com>
2025-07-29 10:22:14 +0845

    Add files via upload

---
+++ Pattern24.java
@@ -0,0 +1,34 @@
+package com.gqt.CoreJava.Pattern;
+
+import java.util.Scanner;
+
+public class Pattern24 {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the size:");
+		int n = sc.nextInt();
+		int rows = n;
+		for (int i = 1; i <= rows ; i++) {
+            
+            for (int space = 1; space <= rows - i; space++) 
+            {
+                System.out.print("  "); 
+            }
+
+         
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                System.out.print(i + " ");
+            }
+
+            System.out.println();
+        }
+    }
+}
+
+
+	
+
+
---
+++ Pattern34.java
@@ -0,0 +1,27 @@
+package com.gqt.CoreJava.Pattern;
+
+public class Pattern34 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 5; 
+        for (int i = 1; i <= n; i++) {
+         
+            for (int s = 1; s <= n - i; s++) {
+                System.out.print("  ");
+            }
+
+            for (char ch = (char) ('A' + i - 1); ch >= 'A'; ch--) {
+                System.out.print(ch + " ");
+            }
+
+            for (char ch = 'B'; ch <= (char) ('A' + i - 1); ch++) {
+                System.out.print(ch + " ");
+            }
+
+            System.out.println(); 
+        }
+
+	}
+
+}
